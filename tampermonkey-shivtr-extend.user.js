66c2ffb5a7ad439a2410e218d425dfedc966c1ee
Jety-Desktop\Josef <st43266@student.upce.cz>
2020-01-06 23:03:34 +0100

    sorting in progress

--- tampermonkey-shivtr-extend.user.js
+++ tampermonkey-shivtr-extend.user.js
@@ -11,8 +11,8 @@
 // @include      *.pwfactions.com/events/*
 // @include      *.shivtr.red/events/*
 // @include      *.shivtr.blue/events/*
-// @updateURL   https://raw.githubusercontent.com/JJetmar/tampermonkey-shivtr-extend/master/tampermonkey-shivtr-extend.user.js
-// @downloadURL https://raw.githubusercontent.com/JJetmar/tampermonkey-shivtr-extend/master/tampermonkey-shivtr-extend.user.js
+// @updateURL    https://raw.githubusercontent.com/JJetmar/tampermonkey-shivtr-extend/master/tampermonkey-shivtr-extend.user.js
+// @downloadURL  https://raw.githubusercontent.com/JJetmar/tampermonkey-shivtr-extend/master/tampermonkey-shivtr-extend.user.js
 // ==/UserScript==
 
 (function() {
@@ -40,6 +40,33 @@
             prompt("Copy to clipboard", result);
         }, false);
     }
+
+    // Sort by groups
+    const result = `/script
+        local m={Jetyus=4,Bonifacek=4,Ledoborec=4,Pythony=4,Aruja=4,Molonlabe=4,Gretorzor=4};
+
+        local g={}; 
+        local pi={};  -- player index mapping => player_name=>raid_index
+
+        for i=1,GetNumGroupMembers() do
+        local p,w,s=GetRaidRosterInfo(i);
+        g[s] = (g[s] and g[s]+1 or 1);
+        pi[p] = i;
+        print(s, g[s]);
+        end
+
+        for i=1,GetNumGroupMembers() do
+        local p=GetRaidRosterInfo(i);
+        if g[m[p]] == 5 then
+            -- find first invalid user in group 
+            -- SwapRaidSubgroup(20, 15);
+        else
+            -- move to group
+            -- SetRaidSubgroup(index, subgroup);
+        end
+    end`.replace(/--[^\n]*/mg, "").replace(/\s+/mg, " ").replace(/; /mg, ";");
+
+
 
     // On page load
     initInvitations();
